ff63394eeb6418f65be0233f3159bd2c5bcf438e
macanthonyoha <137187314+macanthonyoha@users.noreply.github.com>
2024-02-01 17:55:14 +0000

    blogs app and blog model

--- blog_app/admin.py
+++ blog_app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category, Blog
 
-# Register your models here.
+
+# Generating slug title for our blog
+class BlogAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    list_display = ('title', 'category', 'author', 'is_featured', 'status')
+    search_fields = ('title', 'category__category_name', 'status')
+    list_editable = ('is_featured',)
+
+
+admin.site.register(Category)
+admin.site.register(Blog, BlogAdmin)
--- blog_app/models.py
+++ blog_app/models.py
@@ -1,3 +1,40 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=200, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.category_name
+
+    class Meta:
+        verbose_name_plural = "categories"
+
+
+# TO CREATE A DROP-DOWN
+STATUS_CHOICES = (
+    ('Draft', 'Draft'),
+    (1, 'Published'),
+)
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=150, unique=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    # the featured-image will be uploaded to a folder called upload with the current year, month and date
+    feature_image = models.ImageField(upload_to='uploads/%Y/%m/%d/')
+    short_description = models.TextField(max_length=1000)
+    blog_body = models.TextField(max_length=2000)
+    status = models.IntegerField(max_length=20, choices=STATUS_CHOICES, default='Draft')
+    is_featured = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
